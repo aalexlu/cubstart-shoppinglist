0c21fe9c0af180a28cf0719992dd2adae1c7af71
alex <alu@berkeley.edu>
2020-10-15 19:57:49 -0700

    added comments

--- Cubstart-Shopping-List/ItemViewController.swift
+++ Cubstart-Shopping-List/ItemViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 class ItemViewController: UIViewController {
-    
+    /** IBOutlets for each element in our ItemViewController; set in viewDidLoad below */
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var itemCount: UILabel!
 
+    /** variables for passing data from ViewController; set in the prepare function */
     var name = ""
     var image = ""
     var count = 0
     
+    /** viewDidLoad */
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Sets the IBOutlets in the VC to the ones passed in when segue was performed
         self.itemName.text = name
         self.itemCount.text = "Count: \(count)"
         self.itemImage.image = UIImage(named: image)
--- Cubstart-Shopping-List/ViewController.swift
+++ Cubstart-Shopping-List/ViewController.swift
@@ -9,32 +9,41 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
+    /** IBOutlets for each element in our CustomTableViewCell; set in cellForRowAt */
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var itemCount: UILabel!
     @IBOutlet weak var itemImage: UIImageView!
 }
 
 class ViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+    /** IBOutlet for tableView */
     @IBOutlet weak var tableView: UITableView!
+    
+    /** Arrays with our data */
     let itemName = ["Apple", "Orange", "Banana"]
     let itemCount = [4, 10, 3]
     let imageName = ["apple.jpeg", "orange.jpg", "banana.jpg"]
     
+    /** Index of the row clicked by the user */
     var selectedIndex = Int()
     
+    /** viewDidLoad */
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Registers the CustomTableViewCell
         self.tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "custom")
     }
     
+    /** DataSource function: How many rows does this table view have */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // How many elements in our data array
         return itemName.count
     }
     
+    /** Delegate function: For every row in this table view, set the cells to, in this case, in instance of our CustomTableViewCell */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : CustomTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "custom") as! CustomTableViewCell
-        
+        // Sets the labels/image in each row of the Table View; remember each row is a CustomTableViewCell
         cell.itemName.text = itemName[indexPath.row]
         cell.itemCount.text = "Count: \(itemCount[indexPath.row])"
         cell.itemImage.image = UIImage(named: imageName[indexPath.row])
@@ -42,15 +51,18 @@ class ViewController : UIViewController, UITableViewDataSource, UITableViewDeleg
         return cell
     }
 
+    /** Delegate function: when a user selects a row, what happens */
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Saves the row clicked by the user in a variable, so that when we perform/prepare, we can pass the correct data specific to the row clicked
         selectedIndex = indexPath.row
         performSegue(withIdentifier: "item", sender: self)
     }
     
+    /** prepare function passes data from this VC to the ItemVC */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "item" {
             let vc : ItemViewController = segue.destination as! ItemViewController
-            
+            // Using the selectedIndex (set in didSelectRowAt,) set these instance variables in ItemVC to the corresponding data from the arrays
             vc.name = itemName[selectedIndex]
             vc.count = itemCount[selectedIndex]
             vc.image = imageName[selectedIndex]
